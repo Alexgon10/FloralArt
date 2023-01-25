f331341c6eaf6cf731d15c8b0874bff07212a390
Alex <alexgon10@mail.ru>
2023-01-25 16:56:04 +0300

    add backend module

---
+++ floralart-backend/src/main/java/com/example/floralartbackend/FloralartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.floralartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloralartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloralartBackendApplication.class, args);
+    }
+
+}
---
+++ floralart-backend/src/test/java/com/example/floralartbackend/FloralartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floralartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloralartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
